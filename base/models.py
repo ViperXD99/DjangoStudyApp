a097533914ec420aa9c0e73f2650f5ef171b47a5
ViperXD99 <sandaruxd99@gmail.com>
2023-09-05 14:58:18 +0530

    Create operation
    Django CRUD Operation - 1(create room for study app)

--- base/models.py
+++ base/models.py
@@ -1,7 +1,9 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import models  # used to define database models
+from django.contrib.auth.models import User # The built-in 'User' model provided by django for user authentication
 # Create your models here.
 
+
+# Models in django used to represent the database tables
 class Topic(models.Model):
     name =  models.CharField(max_length=200)
 
@@ -19,10 +21,13 @@ class Room (models.Model):
     updated = models.DateTimeField(auto_now= True) # The datetime will updated everytime when save
     created = models.DateTimeField(auto_now_add=True) # The Date Time will updated at the 1st save only
 
+    class Meta:
+        ordering = ['-updated', '-created']
     #Creating the string representation for the class
     def __str__(self):
         return self.name 
     
+
 
 class Message (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
